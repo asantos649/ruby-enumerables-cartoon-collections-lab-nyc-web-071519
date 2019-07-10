2eda54610a7d054f52693acab6d308b080cf49e2
Andrew Santos <asantos@alumni.nd.edu>
2019-07-10 17:27:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(array)
   array.map{|s| s.capitalize() + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.map
 end
 
 def find_the_cheese# code an argument here
