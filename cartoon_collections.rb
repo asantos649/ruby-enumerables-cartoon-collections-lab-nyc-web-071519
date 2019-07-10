ef9004769ca48b643b6781ded3a3f0a52c751042
Andrew Santos <asantos@alumni.nd.edu>
2019-07-10 17:08:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each{i|puts array[string]
 end
 
 def summon_captain_planet# code an argument here
